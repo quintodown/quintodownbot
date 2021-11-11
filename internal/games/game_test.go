3af3fac3589e65adb8f35c2bf3a8937ddec91204
Javi Martin <me@javi.yt>
2021-11-11 15:50:51 +0100

    closes #35 Remove funlen from golangci-lint disabled rules

--- internal/games/game_test.go
+++ internal/games/game_test.go
@@ -17,28 +17,22 @@ import (
 
 func TestGameHandler_GetGames(t *testing.T) {
 	t.Run("it should return an empty list when games shouldn't be initialised", func(t *testing.T) {
-		gic := new(mgms.GameInfoClient)
-		q := new(mps.Queue)
-		mclk := new(mapp.Clock)
-
-		gh := games.NewGameHandler(gic, false, q, mclk)
+		gh := games.NewGameHandler(new(mgms.GameInfoClient), false, new(mps.Queue), new(mapp.Clock))
 
 		require.Empty(t, gh.GetGames(games.NFL))
 	})
 
 	t.Run("it should return an empty list when failed initialising games", func(t *testing.T) {
 		gic := new(mgms.GameInfoClient)
-		q := new(mps.Queue)
-		mclk := new(mapp.Clock)
-
 		initialised := make(chan interface{})
+
 		gic.On("GetGames", games.NFL).Once().Return(func(games.Competition) []games.Game {
 			defer close(initialised)
 
 			return nil
 		}, errors.New("failing"))
 
-		gh := games.NewGameHandler(gic, true, q, mclk)
+		gh := games.NewGameHandler(gic, true, new(mps.Queue), new(mapp.Clock))
 
 		<-initialised
 
@@ -49,9 +43,6 @@ func TestGameHandler_GetGames(t *testing.T) {
 
 	t.Run("it should return a list of games sorted by start date after they have been initialised", func(t *testing.T) {
 		gic := new(mgms.GameInfoClient)
-		q := new(mps.Queue)
-		mclk := new(mapp.Clock)
-
 		initialised := make(chan interface{})
 
 		gic.On("GetGames", games.NFL).Once().Return(func(games.Competition) []games.Game {
@@ -69,7 +60,7 @@ func TestGameHandler_GetGames(t *testing.T) {
 			}
 		}, nil)
 
-		gh := games.NewGameHandler(gic, true, q, mclk)
+		gh := games.NewGameHandler(gic, true, new(mps.Queue), new(mapp.Clock))
 
 		<-initialised
 		require.Eventually(t, func() bool { return len(gic.Calls) > 0 }, time.Second, time.Millisecond)
@@ -169,279 +160,73 @@ func TestGameHandler_UpdateGamesInformation(t *testing.T) {
 	startPlaying := time.Now().UTC().Add(-1 * time.Hour)
 
 	t.Run("it should not send any update when fails getting game update", func(t *testing.T) {
-		gic, _, gh := initialiseGameHandler(t, startPlaying)
-
-		gic.On("GetGameInformation", games.NFL, "asdfg").
-			Once().
-			Return(games.Game{}, errors.New("testing"))
+		gic, _, gh := initGameHandler(t, startPlaying, games.Game{}, errors.New("testing"), "")
 
 		gh.UpdateGamesInformation(true)
-
-		gic.AssertExpectations(t)
-		gic = nil
-		gh = nil
+		mockAssertion(t, gic, nil)
 	})
 
 	t.Run("it should not send any update when there isn't any update", func(t *testing.T) {
-		gic, _, gh := initialiseGameHandler(t, startPlaying)
-
-		gic.On("GetGameInformation", games.NFL, "asdfg").
-			Once().
-			Return(games.Game{
-				Id:          "asdfg",
-				Start:       startPlaying,
-				Status:      games.GameStatus{State: games.InProgressState},
-				Competition: games.NFL,
-			}, nil)
+		gic, _, gh := initGameHandler(t, startPlaying, gameInProgress(startPlaying), nil, "")
 
 		gh.UpdateGamesInformation(true)
-
-		gic.AssertExpectations(t)
-		gic = nil
-		gh = nil
+		mockAssertion(t, gic, nil)
 	})
 
 	t.Run("it should send game information when game has been rescheduled", func(t *testing.T) {
-		gic, q, gh := initialiseGameHandler(t, startPlaying)
-
 		newTime := time.Now().UTC().Add(4 * time.Hour)
 
-		gic.On("GetGameInformation", games.NFL, "asdfg").
-			Once().
-			Return(games.Game{
-				Id:    "asdfg",
-				Start: newTime,
-			}, nil)
-		q.On(
-			"Publish",
-			pubsub.GamesTopic.String(),
-			mock.MatchedBy(func(m *message.Message) bool {
-				return string(m.Payload) == "{\"id\":\"asdfg\","+
-					"\"start\":\""+newTime.Format(time.RFC3339Nano)+"\","+
-					"\"name\":\"\",\"venue\":{\"fullName\":\"\",\"city\":\"\","+
-					"\"state\":\"\",\"capacity\":0,\"indoor\":false},"+
-					"\"status\":{\"clock\":0,\"displayClock\":\"\",\"period\":0,"+
-					"\"state\":\"RescheduledState\"},\"weather\":{\"displayValue\":\"\","+
-					"\"temperature\":0},\"homeTeam\":{\"score\":0,\"name\":\"\","+
-					"\"shortDisplayName\":\"\",\"logo\":\"\",\"record\":\"\"},"+
-					"\"awayTeam\":{\"score\":0,\"name\":\"\",\"shortDisplayName\":\"\","+
-					"\"logo\":\"\",\"record\":\"\"},\"weekName\":\"\","+
-					"\"competition\":\"NFL\",\"lastGameChange\":\"Rescheduled\"}"
-			}),
-		).Once().Return(nil)
+		gic, q, gh := initGameHandler(t, startPlaying, gameRescheduled(newTime), nil, rescheduledPayload(newTime))
 
 		gh.UpdateGamesInformation(true)
-
-		gic.AssertExpectations(t)
-		q.AssertExpectations(t)
-		gic = nil
-		q = nil
-		gh = nil
+		mockAssertion(t, gic, q)
 	})
 
 	t.Run("it should send game information when home team scores", func(t *testing.T) {
-		gic, q, gh := initialiseGameHandler(t, startPlaying)
-
-		gic.On("GetGameInformation", games.NFL, "asdfg").
-			Once().
-			Return(games.Game{
-				Id:       "asdfg",
-				Start:    startPlaying,
-				HomeTeam: games.TeamScore{Score: 7},
-			}, nil)
-		q.On(
-			"Publish",
-			pubsub.GamesTopic.String(),
-			mock.MatchedBy(func(m *message.Message) bool {
-				return string(m.Payload) == "{\"id\":\"asdfg\","+
-					"\"start\":\""+startPlaying.Format(time.RFC3339Nano)+"\",\"name\":\"\","+
-					"\"venue\":{\"fullName\":\"\",\"city\":\"\",\"state\":\"\","+
-					"\"capacity\":0,\"indoor\":false},"+
-					"\"status\":{\"clock\":0,\"displayClock\":\"\",\"period\":0,"+
-					"\"state\":\"InProgressState\"},"+
-					"\"weather\":{\"displayValue\":\"\",\"temperature\":0},"+
-					"\"homeTeam\":{\"score\":7,\"name\":\"\","+
-					"\"shortDisplayName\":\"\",\"logo\":\"\",\"record\":\"\"},"+
-					"\"awayTeam\":{\"score\":0,\"name\":\"\","+
-					"\"shortDisplayName\":\"\",\"logo\":\"\",\"record\":\"\"},"+
-					"\"weekName\":\"\",\"competition\":\"NFL\","+
-					"\"lastGameChange\":\"HomeScore\"}"
-			}),
-		).Once().Return(nil)
+		gic, q, gh := initGameHandler(t, startPlaying, gameHomeScore(startPlaying), nil, homeScorePayload(startPlaying))
 
 		gh.UpdateGamesInformation(true)
-
-		gic.AssertExpectations(t)
-		q.AssertExpectations(t)
-		gic = nil
-		q = nil
-		gh = nil
+		mockAssertion(t, gic, q)
 	})
 
 	t.Run("it should send game information when away team scores", func(t *testing.T) {
-		gic, q, gh := initialiseGameHandler(t, startPlaying)
-
-		gic.On("GetGameInformation", games.NFL, "asdfg").
-			Once().
-			Return(games.Game{
-				Id:       "asdfg",
-				Start:    startPlaying,
-				AwayTeam: games.TeamScore{Score: 7},
-			}, nil)
-		q.On(
-			"Publish",
-			pubsub.GamesTopic.String(),
-			mock.MatchedBy(func(m *message.Message) bool {
-				return string(m.Payload) == "{\"id\":\"asdfg\","+
-					"\"start\":\""+startPlaying.Format(time.RFC3339Nano)+"\",\"name\":\"\","+
-					"\"venue\":{\"fullName\":\"\",\"city\":\"\",\"state\":\"\","+
-					"\"capacity\":0,\"indoor\":false},"+
-					"\"status\":{\"clock\":0,\"displayClock\":\"\",\"period\":0,"+
-					"\"state\":\"InProgressState\"},"+
-					"\"weather\":{\"displayValue\":\"\",\"temperature\":0},"+
-					"\"homeTeam\":{\"score\":0,\"name\":\"\","+
-					"\"shortDisplayName\":\"\",\"logo\":\"\",\"record\":\"\"},"+
-					"\"awayTeam\":{\"score\":7,\"name\":\"\","+
-					"\"shortDisplayName\":\"\",\"logo\":\"\",\"record\":\"\"},"+
-					"\"weekName\":\"\",\"competition\":\"NFL\","+
-					"\"lastGameChange\":\"AwayScore\"}"
-			}),
-		).Once().Return(nil)
+		gic, q, gh := initGameHandler(t, startPlaying, gameAwayScore(startPlaying), nil, awayScorePayload(startPlaying))
 
 		gh.UpdateGamesInformation(true)
-
-		gic.AssertExpectations(t)
-		q.AssertExpectations(t)
-		gic = nil
-		q = nil
-		gh = nil
+		mockAssertion(t, gic, q)
 	})
 
 	t.Run("it should send game information when game has started", func(t *testing.T) {
-		gic, q, gh := initialiseGameHandler(t, startPlaying)
-
-		gic.On("GetGameInformation", games.NFL, "asdfg").
-			Once().
-			Return(games.Game{
-				Id:    "asdfg",
-				Start: startPlaying,
-				Status: games.GameStatus{
-					Period: 1,
-					State:  games.InProgressState,
-				},
-			}, nil)
-		q.On(
-			"Publish",
-			pubsub.GamesTopic.String(),
-			mock.MatchedBy(func(m *message.Message) bool {
-				return string(m.Payload) == "{\"id\":\"asdfg\","+
-					"\"start\":\""+startPlaying.Format(time.RFC3339Nano)+"\",\"name\":\"\","+
-					"\"venue\":{\"fullName\":\"\",\"city\":\"\",\"state\":\"\","+
-					"\"capacity\":0,\"indoor\":false},"+
-					"\"status\":{\"clock\":0,\"displayClock\":\"\",\"period\":1,"+
-					"\"state\":\"InProgressState\"},"+
-					"\"weather\":{\"displayValue\":\"\",\"temperature\":0},"+
-					"\"homeTeam\":{\"score\":0,\"name\":\"\","+
-					"\"shortDisplayName\":\"\",\"logo\":\"\",\"record\":\"\"},"+
-					"\"awayTeam\":{\"score\":0,\"name\":\"\","+
-					"\"shortDisplayName\":\"\",\"logo\":\"\",\"record\":\"\"},"+
-					"\"weekName\":\"\",\"competition\":\"NFL\","+
-					"\"lastGameChange\":\"Started\"}"
-			}),
-		).Once().Return(nil)
+		gic, q, gh := initGameHandler(t, startPlaying, gameStarted(startPlaying), nil, getStartedPayload(startPlaying))
 
 		gh.UpdateGamesInformation(true)
-
-		gic.AssertExpectations(t)
-		q.AssertExpectations(t)
-		gic = nil
-		q = nil
-		gh = nil
+		mockAssertion(t, gic, q)
 	})
 
 	t.Run("it should send game information when period has finished", func(t *testing.T) {
-		gic, q, gh := initialiseGameHandler(t, startPlaying)
-
-		gic.On("GetGameInformation", games.NFL, "asdfg").
-			Once().
-			Return(games.Game{
-				Id:    "asdfg",
-				Start: startPlaying,
-				Status: games.GameStatus{
-					Period: 2,
-					State:  games.InProgressState,
-				},
-			}, nil)
-		q.On(
-			"Publish",
-			pubsub.GamesTopic.String(),
-			mock.MatchedBy(func(m *message.Message) bool {
-				return string(m.Payload) == "{\"id\":\"asdfg\","+
-					"\"start\":\""+startPlaying.Format(time.RFC3339Nano)+"\",\"name\":\"\","+
-					"\"venue\":{\"fullName\":\"\",\"city\":\"\",\"state\":\"\","+
-					"\"capacity\":0,\"indoor\":false},"+
-					"\"status\":{\"clock\":0,\"displayClock\":\"\",\"period\":2,"+
-					"\"state\":\"InProgressState\"},"+
-					"\"weather\":{\"displayValue\":\"\",\"temperature\":0},"+
-					"\"homeTeam\":{\"score\":0,\"name\":\"\","+
-					"\"shortDisplayName\":\"\",\"logo\":\"\",\"record\":\"\"},"+
-					"\"awayTeam\":{\"score\":0,\"name\":\"\","+
-					"\"shortDisplayName\":\"\",\"logo\":\"\",\"record\":\"\"},"+
-					"\"weekName\":\"\",\"competition\":\"NFL\","+
-					"\"lastGameChange\":\"PeriodFinished\"}"
-			}),
-		).Once().Return(nil)
+		gic, q, gh := initGameHandler(t, startPlaying, gameEndPeriod(startPlaying), nil, endPeriodPayload(startPlaying))
 
 		gh.UpdateGamesInformation(true)
-
-		gic.AssertExpectations(t)
-		q.AssertExpectations(t)
-		gic = nil
-		q = nil
-		gh = nil
+		mockAssertion(t, gic, q)
 	})
 
 	t.Run("it should send game information when game has finished", func(t *testing.T) {
-		gic, q, gh := initialiseGameHandler(t, startPlaying)
-
-		gic.On("GetGameInformation", games.NFL, "asdfg").
-			Once().
-			Return(games.Game{
-				Id:     "asdfg",
-				Start:  startPlaying,
-				Status: games.GameStatus{State: games.FinishedState},
-			}, nil)
-		q.On(
-			"Publish",
-			pubsub.GamesTopic.String(),
-			mock.MatchedBy(func(m *message.Message) bool {
-				return string(m.Payload) == "{\"id\":\"asdfg\","+
-					"\"start\":\""+startPlaying.Format(time.RFC3339Nano)+"\",\"name\":\"\","+
-					"\"venue\":{\"fullName\":\"\",\"city\":\"\",\"state\":\"\","+
-					"\"capacity\":0,\"indoor\":false},"+
-					"\"status\":{\"clock\":0,\"displayClock\":\"\",\"period\":0,"+
-					"\"state\":\"FinishedState\"},"+
-					"\"weather\":{\"displayValue\":\"\",\"temperature\":0},"+
-					"\"homeTeam\":{\"score\":0,\"name\":\"\","+
-					"\"shortDisplayName\":\"\",\"logo\":\"\",\"record\":\"\"},"+
-					"\"awayTeam\":{\"score\":0,\"name\":\"\","+
-					"\"shortDisplayName\":\"\",\"logo\":\"\",\"record\":\"\"},"+
-					"\"weekName\":\"\",\"competition\":\"NFL\","+
-					"\"lastGameChange\":\"GameFinished\"}"
-			}),
-		).Once().Return(nil)
+		gic, q, gh := initGameHandler(t, startPlaying, gameFinished(startPlaying), nil, gameFinishedPayload(startPlaying))
 
 		gh.UpdateGamesInformation(true)
-
-		gic.AssertExpectations(t)
-		q.AssertExpectations(t)
-		gic = nil
-		q = nil
-		gh = nil
+		mockAssertion(t, gic, q)
 	})
 }
 
-func initialiseGameHandler(t *testing.T, startPlaying time.Time) (
+func mockAssertion(t *testing.T, gic *mgms.GameInfoClient, q *mps.Queue) {
+	gic.AssertExpectations(t)
+
+	if q != nil {
+		q.AssertExpectations(t)
+	}
+}
+
+func initGameHandler(t *testing.T, startPlaying time.Time, g games.Game, e error, payload string) (
 	*mgms.GameInfoClient,
 	*mps.Queue,
 	*games.GameHandler,
@@ -469,13 +254,181 @@ func initialiseGameHandler(t *testing.T, startPlaying time.Time) (
 			},
 		}
 	}, nil)
+	gic.On("GetGameInformation", games.NFL, "asdfg").Once().Return(g, e)
+
 	mclk.On("Now").Return(time.Now().UTC())
 
 	gh := games.NewGameHandler(gic, true, q, mclk)
+
+	if len(payload) > 0 {
+		q.On(
+			"Publish",
+			pubsub.GamesTopic.String(),
+			mock.MatchedBy(func(m *message.Message) bool {
+				return string(m.Payload) == payload
+			}),
+		).Once().Return(nil)
+	}
 
 	<-initialised
 
 	require.Eventually(t, func() bool { return len(gh.GetGames(games.NFL)) == 2 }, time.Second, time.Millisecond)
 
 	return gic, q, gh
+}
+
+func rescheduledPayload(newTime time.Time) string {
+	return "{\"id\":\"asdfg\"," +
+		"\"start\":\"" + newTime.Format(time.RFC3339Nano) + "\"," +
+		"\"name\":\"\",\"venue\":{\"fullName\":\"\",\"city\":\"\"," +
+		"\"state\":\"\",\"capacity\":0,\"indoor\":false}," +
+		"\"status\":{\"clock\":0,\"displayClock\":\"\",\"period\":0," +
+		"\"state\":\"RescheduledState\"},\"weather\":{\"displayValue\":\"\"," +
+		"\"temperature\":0},\"homeTeam\":{\"score\":0,\"name\":\"\"," +
+		"\"shortDisplayName\":\"\",\"logo\":\"\",\"record\":\"\"}," +
+		"\"awayTeam\":{\"score\":0,\"name\":\"\",\"shortDisplayName\":\"\"," +
+		"\"logo\":\"\",\"record\":\"\"},\"weekName\":\"\"," +
+		"\"competition\":\"NFL\",\"lastGameChange\":\"Rescheduled\"}"
+}
+
+func gameFinishedPayload(startPlaying time.Time) string {
+	return "{\"id\":\"asdfg\"," +
+		"\"start\":\"" + startPlaying.Format(time.RFC3339Nano) + "\",\"name\":\"\"," +
+		"\"venue\":{\"fullName\":\"\",\"city\":\"\",\"state\":\"\"," +
+		"\"capacity\":0,\"indoor\":false}," +
+		"\"status\":{\"clock\":0,\"displayClock\":\"\",\"period\":0," +
+		"\"state\":\"FinishedState\"}," +
+		"\"weather\":{\"displayValue\":\"\",\"temperature\":0}," +
+		"\"homeTeam\":{\"score\":0,\"name\":\"\"," +
+		"\"shortDisplayName\":\"\",\"logo\":\"\",\"record\":\"\"}," +
+		"\"awayTeam\":{\"score\":0,\"name\":\"\"," +
+		"\"shortDisplayName\":\"\",\"logo\":\"\",\"record\":\"\"}," +
+		"\"weekName\":\"\",\"competition\":\"NFL\"," +
+		"\"lastGameChange\":\"GameFinished\"}"
+}
+
+func endPeriodPayload(startPlaying time.Time) string {
+	return "{\"id\":\"asdfg\"," +
+		"\"start\":\"" + startPlaying.Format(time.RFC3339Nano) + "\",\"name\":\"\"," +
+		"\"venue\":{\"fullName\":\"\",\"city\":\"\",\"state\":\"\"," +
+		"\"capacity\":0,\"indoor\":false}," +
+		"\"status\":{\"clock\":0,\"displayClock\":\"\",\"period\":2," +
+		"\"state\":\"InProgressState\"}," +
+		"\"weather\":{\"displayValue\":\"\",\"temperature\":0}," +
+		"\"homeTeam\":{\"score\":0,\"name\":\"\"," +
+		"\"shortDisplayName\":\"\",\"logo\":\"\",\"record\":\"\"}," +
+		"\"awayTeam\":{\"score\":0,\"name\":\"\"," +
+		"\"shortDisplayName\":\"\",\"logo\":\"\",\"record\":\"\"}," +
+		"\"weekName\":\"\",\"competition\":\"NFL\"," +
+		"\"lastGameChange\":\"PeriodFinished\"}"
+}
+
+func getStartedPayload(startPlaying time.Time) string {
+	return "{\"id\":\"asdfg\"," +
+		"\"start\":\"" + startPlaying.Format(time.RFC3339Nano) + "\",\"name\":\"\"," +
+		"\"venue\":{\"fullName\":\"\",\"city\":\"\",\"state\":\"\"," +
+		"\"capacity\":0,\"indoor\":false}," +
+		"\"status\":{\"clock\":0,\"displayClock\":\"\",\"period\":1," +
+		"\"state\":\"InProgressState\"}," +
+		"\"weather\":{\"displayValue\":\"\",\"temperature\":0}," +
+		"\"homeTeam\":{\"score\":0,\"name\":\"\"," +
+		"\"shortDisplayName\":\"\",\"logo\":\"\",\"record\":\"\"}," +
+		"\"awayTeam\":{\"score\":0,\"name\":\"\"," +
+		"\"shortDisplayName\":\"\",\"logo\":\"\",\"record\":\"\"}," +
+		"\"weekName\":\"\",\"competition\":\"NFL\"," +
+		"\"lastGameChange\":\"Started\"}"
+}
+
+func awayScorePayload(startPlaying time.Time) string {
+	return "{\"id\":\"asdfg\"," +
+		"\"start\":\"" + startPlaying.Format(time.RFC3339Nano) + "\",\"name\":\"\"," +
+		"\"venue\":{\"fullName\":\"\",\"city\":\"\",\"state\":\"\"," +
+		"\"capacity\":0,\"indoor\":false}," +
+		"\"status\":{\"clock\":0,\"displayClock\":\"\",\"period\":0," +
+		"\"state\":\"InProgressState\"}," +
+		"\"weather\":{\"displayValue\":\"\",\"temperature\":0}," +
+		"\"homeTeam\":{\"score\":0,\"name\":\"\"," +
+		"\"shortDisplayName\":\"\",\"logo\":\"\",\"record\":\"\"}," +
+		"\"awayTeam\":{\"score\":7,\"name\":\"\"," +
+		"\"shortDisplayName\":\"\",\"logo\":\"\",\"record\":\"\"}," +
+		"\"weekName\":\"\",\"competition\":\"NFL\"," +
+		"\"lastGameChange\":\"AwayScore\"}"
+}
+
+func homeScorePayload(startPlaying time.Time) string {
+	return "{\"id\":\"asdfg\"," +
+		"\"start\":\"" + startPlaying.Format(time.RFC3339Nano) + "\",\"name\":\"\"," +
+		"\"venue\":{\"fullName\":\"\",\"city\":\"\",\"state\":\"\"," +
+		"\"capacity\":0,\"indoor\":false}," +
+		"\"status\":{\"clock\":0,\"displayClock\":\"\",\"period\":0," +
+		"\"state\":\"InProgressState\"}," +
+		"\"weather\":{\"displayValue\":\"\",\"temperature\":0}," +
+		"\"homeTeam\":{\"score\":7,\"name\":\"\"," +
+		"\"shortDisplayName\":\"\",\"logo\":\"\",\"record\":\"\"}," +
+		"\"awayTeam\":{\"score\":0,\"name\":\"\"," +
+		"\"shortDisplayName\":\"\",\"logo\":\"\",\"record\":\"\"}," +
+		"\"weekName\":\"\",\"competition\":\"NFL\"," +
+		"\"lastGameChange\":\"HomeScore\"}"
+}
+
+func gameFinished(startPlaying time.Time) games.Game {
+	return games.Game{
+		Id:     "asdfg",
+		Start:  startPlaying,
+		Status: games.GameStatus{State: games.FinishedState},
+	}
+}
+
+func gameEndPeriod(startPlaying time.Time) games.Game {
+	return games.Game{
+		Id:    "asdfg",
+		Start: startPlaying,
+		Status: games.GameStatus{
+			Period: 2,
+			State:  games.InProgressState,
+		},
+	}
+}
+
+func gameStarted(startPlaying time.Time) games.Game {
+	return games.Game{
+		Id:    "asdfg",
+		Start: startPlaying,
+		Status: games.GameStatus{
+			Period: 1,
+			State:  games.InProgressState,
+		},
+	}
+}
+
+func gameAwayScore(startPlaying time.Time) games.Game {
+	return games.Game{
+		Id:       "asdfg",
+		Start:    startPlaying,
+		AwayTeam: games.TeamScore{Score: 7},
+	}
+}
+
+func gameHomeScore(startPlaying time.Time) games.Game {
+	return games.Game{
+		Id:       "asdfg",
+		Start:    startPlaying,
+		HomeTeam: games.TeamScore{Score: 7},
+	}
+}
+
+func gameRescheduled(newTime time.Time) games.Game {
+	return games.Game{
+		Id:    "asdfg",
+		Start: newTime,
+	}
+}
+
+func gameInProgress(startPlaying time.Time) games.Game {
+	return games.Game{
+		Id:          "asdfg",
+		Start:       startPlaying,
+		Status:      games.GameStatus{State: games.InProgressState},
+		Competition: games.NFL,
+	}
 }
